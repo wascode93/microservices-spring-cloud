b01083f43c8939352db38396a69ce56c6d118167
wascode <waseem.m.salama@gmail.com>
2022-01-02 12:36:21 +0200

    add persistence dependencies and entities

---
+++ microservices/review-service/src/main/java/com/wascode/microservices/core/review/entity/ReviewEntity.java
@@ -0,0 +1,32 @@
+package com.wascode.microservices.core.review.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews")
+public class ReviewEntity {
+
+  @Id @GeneratedValue private int id;
+  @Version private int version;
+
+  private int productId;
+  private int reviewId;
+  private String author;
+  private String subject;
+  private String content;
+
+  public ReviewEntity() {}
+
+  public ReviewEntity(int productId, int reviewId, String author, String subject, String content) {
+    this.productId = productId;
+    this.reviewId = reviewId;
+    this.author = author;
+    this.subject = subject;
+    this.content = content;
+  }
+}
